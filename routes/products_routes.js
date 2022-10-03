5e03a50436e6e831ee557f6682ceb345bd233908
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-10-03 17:50:09 +0530

    MongoDB CRUD Endpoints Created

--- routes/products_routes.js
+++ routes/products_routes.js
@@ -1,7 +1,26 @@
 let product = require('express').Router()
 
+// MongoDB CRUD
+
 product.get("/all", (req, res)=>{
     res.send("/product/all")
+})
+
+product.post("/new", (req, res)=>{
+    res.send("/product/new")
+})
+
+product.put("/update", (req, res)=>{
+    res.send("/product/update")
+})
+
+product.delete("/delete/:id", (req, res)=>{
+    console.log(req.method)
+    res.send(`/product/delete/${req.params.id}`)
+})
+
+product.get("/details/:id", (req, res)=>{
+    res.end(`/product/details/${req.params.id}`)
 })
 
 module.exports = product
