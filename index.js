850247d10634956af239ce5296185642239ac197
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-10-03 17:49:49 +0530

    MySQL CRUD Completed

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ let routes = require('./routes')
 const app = express()
 const port = 8080
 
+app.use(express.json())
 app.use("/", routes)
+
+app.post("/samplepost", (req,res)=>{
+    console.log(req.body)
+    res.send("Sample Post")
+})
 
 app.get("/", (req, res)=>{
     res.send("This is home page")
@@ -22,7 +28,8 @@ app.get("/sample-json", (req, res)=>{
 })
 
 app.use((req, res, next)=>{
-    res.status(404).send("PAGE NOT FOUND!")
+    
+    res.status(404).send("NOT FOUND!")
 })
 
 app.listen(port, ()=>{
--- routes/users_routes.js
+++ routes/users_routes.js
@@ -1,7 +1,48 @@
 let user = require('express').Router()
+let mysql = require('mysql')
+let con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"123123",
+    database:"node_express"
+})
+
+con.connect((err)=>{
+    err ? console.log(err) : console.log("Connection Successful")
+})
+// MySQL CRUD
 
 user.get("/all", (req, res)=>{
-    res.send("/user/all")
+    con.query("SELECT * FROM users", (err, rows, fields)=>{
+        res.send(rows)
+    })
+})
+
+user.get("/details/:id", (req, res)=>{
+    con.query(`SELECT * FROM users WHERE id=${req.params.id}`, (err, rows, fields)=>{
+        res.send(rows)
+    })
+    
+})
+
+user.post("/new", (req,res)=>{
+    let ud = req.body
+    con.query(`INSERT INTO users(name, email, phone, password) VALUES('${ud.name}','${ud.email}','${ud.phone}','${ud.password}')`, (err, rows, fields)=>{
+        err ? res.send("SOME ERROR") : res.send("ONE ROW INSERTED")
+    })
+})
+
+user.put("/update", (req,res)=>{
+    let ud = req.body
+    con.query(`UPDATE users SET name='${ud.name}', email='${ud.email}', phone='${ud.phone}', password='${ud.password}'WHERE id=${ud.id}`, (err, rows, fields)=>{
+        err ? res.send("SOME ERROR") : res.send("ONE ROW UPDATED")
+    })
+})
+
+user.delete("/delete/:id", (req,res)=>{
+    con.query(`DELETE FROM users WHERE id=${req.params.id}`, (err, rows, fields)=>{
+        err ? res.send("SOME ERROR") : res.send("ONE ROW DELETED")
+    })
 })
 
 module.exports = user
